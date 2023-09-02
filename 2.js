a29a810a5077660bb5dbce02d7b08f302e31d004
Rinad <921837@gmail.com>
2023-09-02 13:43:53 +0300

    1

---
+++ 2.js
@@ -0,0 +1,60 @@
+/*""Управление списком заказов""
+
+Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
+
+Свойство orderNumber (номер заказа) - число, уникальный номер заказа.
+Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+Метод addProduct(product) - принимает объект product и добавляет его в список продуктов заказа.
+Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+// Пример использования класса
+class Product {
+constructor(name, price) {
+this.name = name;
+this.price = price;
+}
+}
+
+const order = new Order(12345);
+
+const product1 = new Product(""Phone"", 500);
+order.addProduct(product1);
+
+const product2 = new Product(""Headphones"", 100);
+order.addProduct(product2);
+
+console.log(order.getTotalPrice()); // Вывод: 600*/
+
+class Order {
+  constructor(orderNumber, products) {
+    this.orderNumber = orderNumber;
+    this.products = [products];
+  }
+
+  addProduct(product) {
+    this.products.push(product);
+  }
+
+  getTotalPrice() {
+    let sum = 0;
+    for (let i = 1; i < this.products.length; i++) {
+      sum += this.products[i].price;
+    }
+    return sum;
+  }
+}
+
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+const product1 = new Product("Phone", 500);
+const product2 = new Product("Headphones", 100);
+
+const order = new Order(5);
+order.addProduct(product1);
+order.addProduct(product2);
+console.log(order.getTotalPrice());
